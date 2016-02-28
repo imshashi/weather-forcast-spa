d4e145789ca47874dccf9a377f93880bc9052c9c
imshashi <shashi.aimit@hotmail.com>
2016-02-28 14:00:13 +0530

    add links to get forecast for 2, 5 and 7 days

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ weatherApp.config(function($routeProvider) {
   .when('/forecast', {
     templateUrl: 'pages/forecast.html',
     controller: 'forecastController'
+  })
+  .when('/forecast/:days', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
   });
 });
 
@@ -34,13 +38,14 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$filter', 'cityService', function($scope, $resource, $filter, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$filter', '$routeParams', 'cityService', function($scope, $resource, $filter, $routeParams, cityService) {
 
   $scope.city = cityService.city;
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
-  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, APPID: "7604a9f22b05b9e422a9386de8a5e3df" });
+  $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, APPID: "7604a9f22b05b9e422a9386de8a5e3df" });
 
   $scope.convertToCelcius = function(degK) {
     return degK - 273.15;
