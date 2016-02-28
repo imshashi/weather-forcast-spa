e29fa2f17f44e39f7dd31f88f47d013994825dc4
imshashi <shashi.aimit@hotmail.com>
2016-02-29 01:06:34 +0530

    extract out the directive from app itself and use that

--- app.js
+++ app.js
@@ -52,3 +52,18 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$filter', '
   }
 
 }]);
+
+// DIRECTIVES
+weatherApp.directive('weatherReport', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/weatherReport.html',
+    replace: true,
+    scope: {
+      weatherDay: "=",
+      convertToStandard: "&",
+      convertToDate: "&",
+      dateFormat: "@"
+    }
+  }
+});
