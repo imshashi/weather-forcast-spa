cfc6072b70a72fd3b0886bb4f949f798a7c5560c
imshashi <shashi.aimit@hotmail.com>
2016-02-28 11:21:12 +0530

    add pages and respective routes to app

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 // MODULE
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+weatherApp.config(function($routeProvider) {
+
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeController'
+  })
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.html',
+    controller: 'forecastController'
+  });
+});
+
+// CONTROLLERS
+weatherApp.controller('homeController', ['$scope', function($scope) {
+
+}]);
+
+weatherApp.controller('forecastController', ['$scope', function($scope) {
+
+}]);
