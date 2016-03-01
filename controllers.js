3b28d4c88b99133c07b70afb06a8cab5479ba879
imshashi <shashi.aimit@hotmail.com>
2016-03-01 20:09:08 +0530

    improving UX with ng-submit

--- controllers.js
+++ controllers.js
@@ -1,11 +1,15 @@
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
 
   $scope.city = cityService.city;
 
   $scope.$watch('city', function(){
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function() {
+    $location.path("/forecast");
+  }
 
 }]);
 
